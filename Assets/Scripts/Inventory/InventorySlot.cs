2da4cc6749f2c959abe74762bd19d6217fc47f27
Maxim Merkulov <map20i0253@gmail.com>
2023-04-23 11:46:20 +0300

    Fixed Invenory drag and drop

--- Assets/Scripts/Inventory/InventorySlot.cs
+++ Assets/Scripts/Inventory/InventorySlot.cs
@@ -20,44 +20,45 @@ public class InventorySlot : MonoBehaviour, IDropHandler
 
     public void OnDrop(PointerEventData eventData)
     {
-        //if (IsFreeSlot())
-        //{
-        //    GameObject dropped = eventData.pointerDrag;
-        //    DraggableItem draggableItem = dropped.GetComponent<DraggableItem>();
-        //    draggableItem.ParentAfterDrag = transform;
-        //}
-        //else
-        //{
-        //    Item droppedItem = eventData.pointerDrag.GetComponent<Item>();
-        //    Item currentItem = GetComponentInChildren<Item>();
-        //    if (droppedItem.itemType == currentItem.itemType)
-        //    {
-        //        currentItem.amount += droppedItem.amount;
-        //        Destroy(eventData.pointerDrag);
-        //    }
-        //}
-
-        GameObject dropped = eventData.pointerDrag;
-        Item draggableItem = dropped.GetComponent<Item>();
-        // Если слот не пустой
-        if (!IsFreeSlot())
+        Debug.Log("OnDrop");
+        if (IsFreeSlot())
         {
-            Debug.Log("1");
-            Item item = GetComponentInChildren<Item>();
-            // Если нужный тип предмета
-            if (item.itemType == draggableItem.itemType)
-            {
-                item.amount += draggableItem.amount;
-                Destroy(dropped);
-            }
+            GameObject dropped = eventData.pointerDrag;
+            DraggableItem draggableItem = dropped.GetComponent<DraggableItem>();
+            draggableItem.ParentAfterDrag = transform;
         }
-        // Если же слот пустой
         else
         {
-            DraggableItem draggablObject = dropped.GetComponent<DraggableItem>();
-            draggablObject.ParentAfterDrag = transform;
-            Debug.Log("2");
+            Item droppedItem = eventData.pointerDrag.GetComponent<Item>();
+            Item currentItem = GetComponentInChildren<Item>();
+            if (droppedItem.itemType == currentItem.itemType)
+            {
+                currentItem.amount += droppedItem.amount;
+                Destroy(eventData.pointerDrag);
+            }
         }
+
+        //GameObject dropped = eventData.pointerDrag;
+        //Item draggableItem = dropped.GetComponent<Item>();
+        //// Если слот не пустой
+        //if (!IsFreeSlot())
+        //{
+        //    Debug.Log("1");
+        //    Item item = GetComponentInChildren<Item>();
+        //    // Если нужный тип предмета
+        //    if (item.itemType == draggableItem.itemType)
+        //    {
+        //        item.amount += draggableItem.amount;
+        //        Destroy(dropped);
+        //    }
+        //}
+        //// Если же слот пустой
+        //else
+        //{
+        //    DraggableItem draggablObject = dropped.GetComponent<DraggableItem>();
+        //    draggablObject.ParentAfterDrag = transform;
+        //    Debug.Log("2");
+        //}
     }
 
 
